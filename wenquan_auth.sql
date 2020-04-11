330b222b37ec7d9a49f0b45f7d16eccdbc80dc6b
wenquan <80071319@qq.com>
2020-04-11 18:16:00 +0800

    数据库文件上传

---
+++ wenquan_auth.sql
@@ -0,0 +1,133 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- 主机： 127.0.0.1
+-- 生成日期： 2020-04-11 12:15:07
+-- 服务器版本： 10.4.11-MariaDB
+-- PHP 版本： 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 数据库： `wenquan_auth`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `sq_admin`
+--
+-- 读取表 wenquan_auth.sq_admin 的结构时出错： #1932 - Table 'wenquan_auth.sq_admin' doesn't exist in engine
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `sq_agent`
+--
+-- 读取表 wenquan_auth.sq_agent 的结构时出错： #1932 - Table 'wenquan_auth.sq_agent' doesn't exist in engine
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `sq_apps`
+--
+-- 读取表 wenquan_auth.sq_apps 的结构时出错： #1932 - Table 'wenquan_auth.sq_apps' doesn't exist in engine
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `sq_bclist`
+--
+-- 读取表 wenquan_auth.sq_bclist 的结构时出错： #1932 - Table 'wenquan_auth.sq_bclist' doesn't exist in engine
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `sq_config`
+--
+-- 读取表 wenquan_auth.sq_config 的结构时出错： #1932 - Table 'wenquan_auth.sq_config' doesn't exist in engine
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `sq_fidkey`
+--
+-- 读取表 wenquan_auth.sq_fidkey 的结构时出错： #1932 - Table 'wenquan_auth.sq_fidkey' doesn't exist in engine
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `sq_fidlist`
+--
+-- 读取表 wenquan_auth.sq_fidlist 的结构时出错： #1932 - Table 'wenquan_auth.sq_fidlist' doesn't exist in engine
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `sq_key`
+--
+-- 读取表 wenquan_auth.sq_key 的结构时出错： #1932 - Table 'wenquan_auth.sq_key' doesn't exist in engine
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `sq_level`
+--
+-- 读取表 wenquan_auth.sq_level 的结构时出错： #1932 - Table 'wenquan_auth.sq_level' doesn't exist in engine
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `sq_log_agent`
+--
+-- 读取表 wenquan_auth.sq_log_agent 的结构时出错： #1932 - Table 'wenquan_auth.sq_log_agent' doesn't exist in engine
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `sq_log_kami`
+--
+-- 读取表 wenquan_auth.sq_log_kami 的结构时出错： #1932 - Table 'wenquan_auth.sq_log_kami' doesn't exist in engine
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `sq_log_system`
+--
+-- 读取表 wenquan_auth.sq_log_system 的结构时出错： #1932 - Table 'wenquan_auth.sq_log_system' doesn't exist in engine
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `sq_token`
+--
+-- 读取表 wenquan_auth.sq_token 的结构时出错： #1932 - Table 'wenquan_auth.sq_token' doesn't exist in engine
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `sq_trade`
+--
+-- 读取表 wenquan_auth.sq_trade 的结构时出错： #1932 - Table 'wenquan_auth.sq_trade' doesn't exist in engine
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `sq_user`
+--
+-- 读取表 wenquan_auth.sq_user 的结构时出错： #1932 - Table 'wenquan_auth.sq_user' doesn't exist in engine
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
