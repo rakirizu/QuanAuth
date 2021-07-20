ee39e3b492137fbc99cf6f84ae3682bc750f8fb5
xiaodaii <1259102089@qq.com>
2021-07-20 08:15:50 +0800

    修复vaptcha验证

--- agent/ajax.php
+++ agent/ajax.php
@@ -8,8 +8,8 @@
  */
 include '../function/function_core.php';
 if ($_GET['mod'] === 'login') {
-    if (!empty($_POST['token'])) {
-        if (!$result = $db->select_first_row('sq_agent', '*', array('accesstoken' => $_POST['token']), 'AND')) {
+    if (!empty($_POST['accesstoken'])) {
+        if (!$result = $db->select_first_row('sq_agent', '*', array('accesstoken' => $_POST['accesstoken']), 'AND')) {
             die(json_encode(array('code' => '-1', 'msg' => '秘钥错误')));
         } else {
             $_SESSION['agent_username'] = $_POST['username'];
